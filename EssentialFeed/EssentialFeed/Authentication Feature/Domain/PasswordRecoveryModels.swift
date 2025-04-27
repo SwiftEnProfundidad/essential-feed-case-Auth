64ec3ec47f117c31c1995c080723db4f8a32f6f6
Swift En Profundidad <freelancemerlos@gmail.com>
2025-04-27 17:43:59 +0200

    Add password recovery feature with models, use case and tests

---
+++ EssentialFeed/EssentialFeed/Authentication Feature/Domain/PasswordRecoveryModels.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+// MARK: - DTOs para recuperación de contraseña
+
+public struct PasswordRecoveryRequest {
+    public let email: String
+    public init(email: String) {
+        self.email = email
+    }
+}
+
+public struct PasswordRecoveryResponse: Equatable {
+    public let message: String
+    public init(message: String) {
+        self.message = message
+    }
+}
+
+public enum PasswordRecoveryError: Error, Equatable {
+    case invalidEmailFormat
+    case emailNotFound
+    case network
+    // Puedes añadir más errores según el backend
+}
+
+// MARK: - Protocolo de API para recuperación de contraseña
+
+public protocol PasswordRecoveryAPI {
+    func recover(email: String, completion: @escaping (Result<PasswordRecoveryResponse, PasswordRecoveryError>) -> Void)
+}
