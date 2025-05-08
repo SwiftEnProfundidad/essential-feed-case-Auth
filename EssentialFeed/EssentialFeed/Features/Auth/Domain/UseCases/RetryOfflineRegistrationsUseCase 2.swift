de524331350ae258ade4ef18d1c54fcf3184f86a
Swift En Profundidad <freelancemerlos@gmail.com>
2025-05-08 17:59:29 +0200

    feat: add RetryOfflineRegistrationsUseCase with test scaffolding

---
+++ EssentialFeed/EssentialFeed/Features/Auth/Domain/UseCases/RetryOfflineRegistrationsUseCase 2.swift
@@ -0,0 +1,10 @@
+//	
+// Copyright © 2025 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+public final class RetryOfflineRegistrationsUseCase {
+	// TODO: Definir dependencias e inicializador
+	// TODO: Definir método execute()
+}
