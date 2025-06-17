7abd291c10f547bb96a6d99cc881433fed569671
Swift En Profundidad <freelancemerlos@gmail.com>
2025-06-17 09:14:20 +0200

    refactor: Removed insecure UserDefaults storage for tokens/credentials 2. Removed login debug print statements and console notifier

--- EssentialApp/EssentialApp/Features/Auth/Routing/BasicLoginFlowHandler.swift
+++ EssentialApp/EssentialApp/Features/Auth/Routing/BasicLoginFlowHandler.swift
@@ -8,21 +8,18 @@ class BasicLoginFlowHandler: LoginFlowHandler {
     func handlePostLogin(result: Result<LoginResponse, LoginError>, credentials _: LoginCredentials) async {
         switch result {
         case .success:
-            print("FlowHandler: Login successful. Calling onAuthenticated.")
             onAuthenticated?()
         case let .failure(error):
-            print("FlowHandler: Login failed with error: \(error). Not calling onAuthenticated.")
-            // Aquí se podría manejar la presentación de errores específicos en la UI si fuera necesario
+            break
         }
     }
 
     func handlePostLogout(result: Result<Void, Error>) async {
         switch result {
         case .success:
-            print("FlowHandler: Logout successful.")
-        // Aquí se podría manejar la navegación a la pantalla de login, por ejemplo.
+            break
         case let .failure(error):
-            print("FlowHandler: Logout failed with error: \(error).")
+            break
         }
     }
 }
