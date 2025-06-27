abcf6554f9369ec4a0501a69e00a79ff96bad04d
Swift En Profundidad <freelancemerlos@gmail.com>
2025-06-27 11:26:01 +0200

    feat: implement password validation with DefaultPasswordValidator and tests

--- EssentialFeed/EssentialFeedTests/FeaturesTest/AuthTest/LoginTest/DomainTest/Services/PasswordValidatorTests.swift
+++ EssentialFeed/EssentialFeedTests/FeaturesTest/AuthTest/LoginTest/DomainTest/Services/PasswordValidatorTests.swift
@@ -3,15 +3,12 @@ import XCTest
 
 final class PasswordValidatorTests: XCTestCase {
     func test_validate_failsWithTooShortError_whenPasswordIsLessThan8Characters() {
-        // Arrange
         let sut = DefaultPasswordValidator()
         let shortPassword = "Short1!"
         let expectedError = PasswordValidationError.tooShort
 
-        // Act
         let result = sut.validate(password: shortPassword)
 
-        // Assert
         switch result {
         case .success:
             XCTFail("Validation should have failed with error \(expectedError), but succeeded instead.")
